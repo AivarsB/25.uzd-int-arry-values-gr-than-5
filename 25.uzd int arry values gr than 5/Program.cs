ddc4596368fc1a947f586d9f891f6e6a4cee4d6c
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 15:47:01 +0200

    Add project files.

---
+++ 25.uzd int arry values gr than 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _25.uzd_int_arry_values_gr_than_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 3, 4, 8, 0, 23 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Values greater than 5:");
+
+            foreach (var number in numbers)
+            {
+                if (number > 5)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
+
+
+// Deklarēt int[] - masīvu int[] numbers = { 1, 3, 4, 8, 0, 23 }; 
+// Izdrukāt uz ekrāna visas masīva elementu vērtības un pēc tam tikai tās vērtības, kuras lielākas par 5.
